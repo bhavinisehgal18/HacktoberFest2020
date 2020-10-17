3d1de5f0b11b7c9e000d99abb91abc5aa9edb3f9
bhavinisehgal18 <62087213+bhavinisehgal18@users.noreply.github.com>
2020-10-17 15:48:23 +0530

    Updated bubble_sort.cpp

--- c++/Bubble_sort.cpp
+++ c++/Bubble_sort.cpp
@@ -1,41 +1,54 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void swapping(int &a, int &b) {      //swap the content of a and b
-   int temp;
-   temp = a;
-   a = b;
-   b = temp;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
-void display(int *array, int size) {
-   for(int i = 0; i<size; i++)
-      cout << array[i] << " ";
-   cout << endl;
-}
-void bubbleSort(int *array, int size) {
-   for(int i = 0; i<size; i++) {
-      int swaps = 0;         //flag to detect any swap is there or not
-      for(int j = 0; j<size-i-1; j++) {
-         if(array[j] > array[j+1]) {       //when the current item is bigger than next
-            swapping(array[j], array[j+1]);
-            swaps = 1;    //set swap flag
-         }
+
+void bubble(int arr[], int i,int n)
+{
+      for(int j=0; j<n-i-1;j++)
+      {
+          if(arr[j]>arr[j+1])
+          {
+              swap(&arr[j],&arr[j+1]);
+          }
       }
-      if(!swaps)
-         break;       // No swap in this pass, so array is sorted
-   }
 }
-int main() {
-   int n;
-   cout << "Enter the number of elements: ";
-   cin >> n;
-   int arr[n];     //create an array with given number of elements
-   cout << "Enter elements:" << endl;
-   for(int i = 0; i<n; i++) {
-      cin >> arr[i];
-   }
-   cout << "Array before Sorting: ";
-   display(arr, n);
-   bubbleSort(arr, n);
-   cout << "Array after Sorting: ";
-   display(arr, n);
+ 
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)      
+        bubble(arr, i, n);
 }
+ 
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout<<arr[i];
+    cout<<endl;
+}
+ 
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    cin>>T;
+    
+    while(T--){
+        
+     cin>>n;
+    
+    for(i=0;i<n;i++)
+      cin>>arr[i];
+      
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;;
+}
+
